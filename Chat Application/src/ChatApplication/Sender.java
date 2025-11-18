f9e9fbc7560030475f095b418ecc32529270095b
ShameelShajaad <shameels1228@gmail.com>
2025-11-18 21:50:26 +0530

    Added sender class

---
+++ Chat Application/src/ChatApplication/Sender.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ChatApplication;
+
+/**
+ *
+ * @author Shameel Shajaad
+ */
+public class Sender {
+    
+}
